25e4892b74f6bc9a96c180704d42fd919a6bd829
Tithi <world.tithi@gmail.com>
2021-12-30 12:39:39 +0600

    Project1
    Javascript, css, html, bootstrap, jquery

---
+++ JS_PROJECT-1/main.js
@@ -0,0 +1,37 @@
+/*const person ={
+
+	name: 'Tithi',
+	birthday:'01/01/1998' ,
+	gender:'Female'
+	
+
+}
+
+document.getElementById('html').innerHTML = person;
+console.log(person);
+*/
+
+
+
+function detail1() {
+ alert("Name: Tithi"+ "\n" + "Birthday:01/01/1998" + "\n" + "Gender: Female"+ "\n" + "Ocupation: Student" + "\n" + "Nationality:Bangladeshi" );
+}
+
+function detail2() {
+ alert("Name: Ridu"+ "\n" + "Birthday:01/01/1998" + "\n" + "Gender: Male"+ "\n" + "Ocupation: Student" + "\n" + "Nationality:Bangladeshi" );
+}
+
+function detail3() {
+ alert("Name: Dipto"+ "\n" + "Birthday:01/01/1999" + "\n" + "Gender: Male"+ "\n" + "Ocupation: Student" + "\n" + "Nationality:Bangladeshi" );
+}
+
+function detail4() {
+ alert("Name: Banani"+ "\n" + "Birthday:01/01/1998" + "\n" + "Gender: Female"+ "\n" + "Ocupation: Student" + "\n" + "Nationality:Bangladeshi" );
+}
+
+function detail5() {
+ alert("Name: Mumu"+ "\n" + "Birthday:01/01/1997" + "\n" + "Gender: Female"+ "\n" + "Ocupation: Student" + "\n" + "Nationality:Bangladeshi" );
+}
+function detail6() {
+ alert("Name: Hasibul Hasan"+ "\n" + "Birthday:01/01/1999" + "\n" + "Gender: Male"+ "\n" + "Ocupation: Student" + "\n" + "Nationality:Bangladeshi" );
+}
